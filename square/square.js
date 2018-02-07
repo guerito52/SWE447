70b2db815438ed0910ccb8f7816b396cc10a8511
Edgar H <edgar1192@gmail.com>
2018-02-06 20:35:29 -0800

    Update square.js

--- square/square.js
+++ square/square.js
@@ -1,33 +1,26 @@
 var square = null;
 var gl = null;
-var angle = 0;
 
 function init() {
 	var canvas = document.getElementById("webgl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
 	
 
-    	if ( !gl ) {
-        	alert("Unable to setup WebGL");
-        	return;
-    	}
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
 
-    	gl.clearColor(  0.8, 0.8, 0.8, 1.0 );
-	gl.enable( gl.DEPTH_TEST );
+    gl.clearColor( 0.0, 1.0, 1.0, 1.0 );
 	
-   	square = new Square();
+    square = new Square(gl);
 	
-   	render();
+    render();
 }
 
 function render() {
-	gl.clear(gl.COLOR_BUFFER_BIT);
-    	angle += 2.0;
-	square.MV = rotate ( angle, [1,1,0]);
-	square.render();
-	
-	requestAnimationFrame( render );
-	
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    square.render();
 }
 
 window.onload = init;
