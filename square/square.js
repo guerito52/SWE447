d6bb86d85e4f78ebbda6bfb6d42eab2bb2967c27
Edgar H <edgar1192@gmail.com>
2018-02-06 18:17:51 -0800

    Update square.js

--- square/square.js
+++ square/square.js
@@ -6,21 +6,21 @@ function init() {
 	gl = WebGLUtils.setupWebGL(canvas);
 	
 
-    if ( !gl ) {
-        alert("Unable to setup WebGL");
-        return;
-    }
+    	if ( !gl ) {
+        	alert("Unable to setup WebGL");
+        	return;
+    	}
 
-    gl.clearColor( 0.0, 1.0, 1.0, 1.0 );
+    	gl.clearColor( 0.0, 1.0, 1.0, 1.0 );
 	
-    square = new Square(gl);
+   	 square = new Square(gl);
 	
-    render();
+   	 render();
 }
 
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    square.render();
+	gl.clear(gl.COLOR_BUFFER_BIT);
+    	square.render();
 }
 
 window.onload = init;
