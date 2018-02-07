1e3d82349b2b7dffcfc58ac0a03cf5c40ab96e7b
Edgar H <edgar1192@gmail.com>
2018-02-06 20:36:07 -0800

    Update Square.js

--- square/Square.js
+++ square/Square.js
@@ -5,25 +5,30 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 	this.program = initShaders(gl, vertShdr, fragShdr);
 
 	if ( this.program < 0 ) {
-    		alert( "Error: Square shader pipeline failed to compile.\n\n" + "\tvertex shader id:  \t" + vertShdr + "\n" + "\tfragment shader id:\t" + fragShdr + "\n" );
-		return; 
+    	alert( "Error: Square shader pipeline failed to compile.\n\n" +
+    	    "\tvertex shader id:  \t" + vertShdr + "\n" +
+    	    "\tfragment shader id:\t" + fragShdr + "\n" );
+    	return; 
 	}
+
+
+	gl.useProgram(this.program);
+	this.count = 4;
 
 	this.positions = {
 		values : new Float32Array([
-		    	// Front face
+		    // Front face
 		    0.0, 0.0, // Vertex 0
 		    1.0, 0.0, // Vertex 1
 		    1.0, 1.0, // Vertex 2
 		    0.0, 1.0  // Vertex 3
-			
 		]),
 		numComponents : 2 // 3 components for each
 		// position (2D coords)
 	};
 	this.colors = {
 		values : new Float32Array([
-		    	1.0, 0.0, 0.0, 
+			1.0, 0.0, 0.0, 
 		    	1.0, 0.0, 0.0,
 		    	1.0, 0.0, 0.0,
 		    	1.0, 0.0, 0.0
@@ -32,51 +37,52 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 	};
     	this.indices = {
     		values : new Uint16Array([ 
-			0, 1, 2,
-			2, 1, 3,
-			2, 3, 4,
-			4, 3, 5,
-			4, 5, 6,
-			5, 7, 6,
-			7, 0, 6,
-			7, 1, 0,
-			3, 1, 7,
-			5, 3, 7,
-			6, 0, 2,
-			6, 2, 4 
-		])
-    	};
-	this.indices.count = this.indices.values.length;
+	    		0,1,2,
+			0,2,3
+    		])
+	};
 	
 	// positions
 	
 	this.positions.buffer = gl.createBuffer();
-    	gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+	gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
     	gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
+	this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
+	gl.enableVertexAttribArray( this.positions.attributeLoc );
 
+	// colors
+    	this.colors.buffer = gl.createBuffer();
+    	gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+    	gl.bufferData( gl.ARRAY_BUFFER, this.colors.values, gl.STATIC_DRAW );
+    	this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor" );
+    	gl.enableVertexAttribArray( this.colors.attributeLoc );
+    
+	// indices
     	this.indices.buffer = gl.createBuffer();
     	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
     	gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, this.indices.values, gl.STATIC_DRAW );
+    
 
-    	this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
-    	gl.enableVertexAttribArray( this.positions.attributeLoc );
+	this.render = function () {
+    		gl.useProgram( this.program );
+    		//gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    	MVLoc = gl.getUniformLocation( this.program, "MV" );
+    		gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+    		gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
+        		gl.FLOAT, gl.FALSE, 0, 0 );
 
-    	this.MV = undefined;
+           
+    		gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+    		gl.vertexAttribPointer( this.colors.attributeLoc, this.colors.numComponents,
+        		gl.FLOAT, gl.FALSE, 0, 0 );
+	
+        	//gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+		var start = 0;
+		var count = this.count;
+		//gl.drawArrays(gl.TRIANGLE_STRIP, start, count); // TRIANGLE_STRIP
+		gl.drawElements(gl.TRIANGLE, this.indices.values.length, gl.UNSIGNED_SHORT, 0);
 
-    	this.render = function () {
-        	gl.useProgram( this.program );
 
-        	gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
-        	gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents, gl.FLOAT, gl.FALSE, 0, 0 );
- 
-        	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+	};
 
-        	gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
-
-        	// Draw the cube's base
-        	gl.drawElements( gl.TRIANGLES, this.indices.count, gl.UNSIGNED_SHORT, 0 );
-    	}
 };
-
