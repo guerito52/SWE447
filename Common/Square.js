2556124ef42a559db25430efbe2f9f5cb4d1c349
Edgar Hernandez <edgar1192@gmail.com>
2018-02-12 23:43:59 -0800

    updated Cube.js

--- Common/Square.js
+++ Common/Square.js
@@ -29,15 +29,15 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 	this.colors = {
 		values : new Float32Array([
 			1.0, 0.0, 0.0, 
-		    	1.0, 0.0, 0.0,
-		    	1.0, 0.0, 0.0,
-		    	1.0, 0.0, 0.0
+		    1.0, 0.0, 0.0,
+		    1.0, 0.0, 0.0,
+		    1.0, 0.0, 0.0
 		]),
 		numComponents : 3 
 	};
     	this.indices = {
     		values : new Uint16Array([ 
-	    		0, 1, 2,
+	    	0, 1, 2,
 			0, 2, 3
     		])
 	};
